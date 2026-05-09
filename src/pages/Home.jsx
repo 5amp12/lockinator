1604155beac86f96e5a033cb22ccb7e0fb38dc00
srgp2 <srgp2@kent.ac.uk>
2026-05-09 11:46:11 +0100

    feat: generic home setup

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import PostureDetector from './PostureDetector'
+ 
+function Home() {
+  return (
+    <div>
+      <h1>Phone Posture Detector</h1>
+      <PostureDetector />
+    </div>
+  )
+}
+ 
+export default Home
+ 
