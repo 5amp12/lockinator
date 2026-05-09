392ae7c36afa42dbf632274f80c600b98f02756a
srgp2 <srgp2@kent.ac.uk>
2026-05-09 13:29:14 +0100

    feat: detects slouching

--- src/pages/PostureDetector.jsx
+++ src/pages/PostureDetector.jsx
@@ -6,10 +6,12 @@ function PostureDetector() {
   const canvasRef         = useRef(null)
   const streamRef         = useRef(null)
   const rafRef            = useRef(null)
-  const poseLandmarkerRef = useRef(null)  // ← add this
+  const poseLandmarkerRef = useRef(null)
+  const postureRef = useRef(true)
 
   const [running, setRunning] = useState(false)
-  const [tilt,    setTilt]    = useState(null)
+  const [tilt, setTilt] = useState(null)
+  const [posture, setPosture] = useState(true)
 
   async function start() {
     const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false })
@@ -35,7 +37,7 @@ function PostureDetector() {
     rafRef.current = requestAnimationFrame(detect)  // ← start loop after model ready
   }
 
-  async function detect() {  // ← async
+  async function detect() { 
     const video  = videoRef.current
     const canvas = canvasRef.current
     const ctx    = canvas.getContext('2d')
@@ -45,23 +47,42 @@ function PostureDetector() {
     const result = await poseLandmarkerRef.current.detectForVideo(video, performance.now())  // ← ref
     if (result.landmarks[0]) {
       const landmarks = result.landmarks[0]
-      console.log(landmarks)
       drawOverlay(ctx, landmarks, canvas.width, canvas.height)
-      // TODO: drawOverlay(ctx, landmarks)
-      // TODO: setTilt(computeTilt(landmarks))
+      if (postureRef.current){
+        computePosture(landmarks);
+      }
+      
     }
 
     rafRef.current = requestAnimationFrame(detect)
   }
 
+  //setting skeletion lines overlay
   function drawOverlay(ctx, landmarks, canvasW, canvasH) {
-  landmarks.forEach(pt => {
-    ctx.beginPath()
-    ctx.arc(pt.x * canvasW, pt.y * canvasH, 5, 0, 2 * Math.PI)
-    ctx.fillStyle = 'red'
-    ctx.fill()
-  })
-}
+    landmarks.forEach(pt => {
+        ctx.beginPath()
+        ctx.arc(pt.x * canvasW, pt.y * canvasH, 5, 0, 2 * Math.PI)
+        ctx.fillStyle = 'red'
+        ctx.fill()
+    })
+  } 
+
+  function computePosture(landmarks) {
+    const nose = landmarks[0]
+    const leftShoulder = landmarks[11]
+    const rightShoulder = landmarks[12]
+
+    const averageShoulderZ = (leftShoulder.z + rightShoulder.z) / 2;
+    const headz = nose.z;
+
+
+    const diff = (averageShoulderZ - headz) / Math.abs(averageShoulderZ)  // positive = head is closer than shoulders = slouching
+    console.log(diff);
+    if (diff < 1.5) {      
+        console.log("Slouching!!")
+    }
+  }
+
 
   function stop() {
     cancelAnimationFrame(rafRef.current)
@@ -78,6 +99,13 @@ function PostureDetector() {
       <p>Head tilt: {tilt !== null ? `${tilt}°` : '–'}</p>
       <button onClick={start} disabled={running}>Start</button>
       <button onClick={stop}  disabled={!running}>Stop</button>
+      {/* <label class="switch"> */}
+      <input type="checkbox" checked={posture} onChange={() => {
+            postureRef.current = !postureRef.current
+            setPosture(p => !p)
+       }} />
+        
+
     </div>
   )
 }
